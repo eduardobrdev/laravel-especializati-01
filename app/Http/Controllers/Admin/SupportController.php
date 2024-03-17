062dbd72a206a5f0eb1efca77de4578437e72c6e
Eduardo Alexandre <edubrdev@gmail.com>
2024-03-17 18:31:56 -0300

    feat: create store function

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -13,4 +13,17 @@ class SupportController extends Controller
         $supports = $support->all();
         return view('admin/supports/index', compact('supports'));
     }
+
+    public function create()
+    {
+        return view('admin/supports/create');
+    }
+
+    public function store(Request $request, Support $support)
+    {
+        $data = $request->all();
+        $data['status'] = 'a';
+        $support->create($data);
+        return redirect()->route('supports.index');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\Admin\SupportController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/supports', [SupportController::class, 'index'])->name('supports,index');
+Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
+Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
+Route::post('supports', [SupportController::class, 'store'])->name('supports.store');
 
 Route::get('/', function () {
     return view('welcome');
